d5a6a346a01c02c7a292c16dba4694971324d760
Phani Vankadari <phani.kumar.vankadari@gmail.com>
2019-05-21 12:17:28 +0000

    dd

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,35 +1,34 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
-	"use strict";
+			"sap/ui/core/mvc/Controller",
+			"sap/m/ObjectListItem"
+		], function (Controller,ObjectListItem) {
+			"use strict";
 
-	return Controller.extend("com.odata.nw.OdataModelSample.controller.Main", {
-		onInit: function () {
+			return Controller.extend("com.odata.nw.OdataModelSample.controller.Main", {
+				onInit: function () {
 
-		},
-		onCategoryChange:function(e){
-			console.log('dbg');
-			var context =e.getParameters().selectedItem.getBindingContext('NWM');
-			
-			var oHCategory = this.getView().byId('oHCategory');
-			if(oHCategory !== undefined){
-				oHCategory.setBindingContext(context,'NWM');
-			}
-			var list = this.getView().byId('listPrs');
-				list.unbindItems();
-				
-			list.bindItems(	{
-								path:context.getPath()+'/Products/',
-								template:this.getView().byId('oli')
+				},
+				onCategoryChange: function (e) {
+					
+					var context = e.getParameters().selectedItem.getBindingContext('NWM');
+
+					var oHCategory = this.getView().byId('oHCategory');
+						var oList = this.getView().byId('listPrs');
 						
-							}
-						);
-			
-			
-		},
-		debug:function(){
-			
-			
-		}
-	});
-});
+						
+					if (oHCategory !== undefined) {
+						oHCategory.setBindingContext(context, 'NWM');
+							oList.setBindingContext(context, 'NWM');
+					}
+					
+				//	list.unbindItems();
+					this.debug();
+				
+
+
+						},
+						debug:function () {
+									console.log('dbg');
+						}
+					});
+			});
