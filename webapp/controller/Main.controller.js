e8d0b3c35c72ff9be882498698775a1be0948664
Phani Vankadari <phani.kumar.vankadari@gmail.com>
2019-05-21 12:16:56 +0000

    odatamodel

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -6,6 +6,30 @@ sap.ui.define([
 	return Controller.extend("com.odata.nw.OdataModelSample.controller.Main", {
 		onInit: function () {
 
+		},
+		onCategoryChange:function(e){
+			console.log('dbg');
+			var context =e.getParameters().selectedItem.getBindingContext('NWM');
+			
+			var oHCategory = this.getView().byId('oHCategory');
+			if(oHCategory !== undefined){
+				oHCategory.setBindingContext(context,'NWM');
+			}
+			var list = this.getView().byId('listPrs');
+				list.unbindItems();
+				
+			list.bindItems(	{
+								path:context.getPath()+'/Products/',
+								template:this.getView().byId('oli')
+						
+							}
+						);
+			
+			
+		},
+		debug:function(){
+			
+			
 		}
 	});
 });
